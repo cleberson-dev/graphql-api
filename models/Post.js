a8cccbcaf98c341aad1bc0dc7af30eb45135836a
cleberson-dev <cleberson.dev@gmail.com>
2020-06-21 12:10:20 -0300

    Definido modelo Postagem (Post)

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const Sequelize, { Model } = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
+
+const User = require('./User'); 
+
+class Post extends Model {}
+Post.init({
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    defaultValue: uuidv4
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      max: 50 
+    }
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      min: 20
+    }
+  },
+  userId: {
+    type: Sequelize.STRING,
+    references: User,
+    key: 'id',
+    deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+  }
+});
