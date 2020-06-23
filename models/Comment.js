99c4df5fc9517da619b5b19d45141e9163321a7a
cleberson-dev <cleberson.dev@gmail.com>
2020-06-22 23:08:29 -0300

    Corrigido importação do banco de dados

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = require('../db');
+const db = require('../config/db');
 const User = require('./User');
 const Post = require('./Post');
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = require('../db');
+const db = require('../config/db');
 
 const User = db.define('user', {
   id: {
